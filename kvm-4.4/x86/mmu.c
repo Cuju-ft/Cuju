eed396fc2de14fedebf6dd08fcdf2cb40e33aba8
PJ Tsao <pjtsao@itri.org.tw>
2021-03-04 18:13:20 +0800

    fix: Fixed the difference optimization problem on SMP system
    Follow the same modification method as the kernel 4.15

--- kvm-4.4/x86/mmu.c
+++ kvm-4.4/x86/mmu.c
@@ -3638,6 +3638,14 @@ static int tdp_page_fault(struct kvm_vcpu *vcpu, gva_t gpa, u32 error_code,
 
 	MMU_WARN_ON(!VALID_PAGE(vcpu->arch.mmu.root_hpa));
 
+    if (kvm_shm_is_enabled(vcpu->kvm)){
+            hva = gfn_to_hva(vcpu->kvm, gfn);
+            if (!kvm_is_error_hva(hva)) {
+            //If this hva is valid, this case is write protect page fault, we can backup page and mark dirty
+            kvmft_page_dirty(vcpu->kvm, gfn, (void *)hva, 1, NULL);
+            }
+    }
+
 	if (unlikely(error_code & PFERR_RSVD_MASK)) {
 		r = handle_mmio_page_fault(vcpu, gpa, true);
 
@@ -3689,14 +3697,6 @@ static int tdp_page_fault(struct kvm_vcpu *vcpu, gva_t gpa, u32 error_code,
 
 	if (handle_abnormal_pfn(vcpu, 0, gfn, pfn, ACC_ALL, &r))
 		return r;
-
-	if (kvm_shm_is_enabled(vcpu->kvm)){
-            hva = gfn_to_hva(vcpu->kvm, gfn);
-            if (!kvm_is_error_hva(hva)) {
-	        //If this hva is valid, this case is write protect page fault, we can backup page and mark dirty
-	        kvmft_page_dirty(vcpu->kvm, gfn, (void *)hva, 1, NULL);
-            }
-	}
 
 	spin_lock(&vcpu->kvm->mmu_lock);
 	if (mmu_notifier_retry(vcpu->kvm, mmu_seq))
